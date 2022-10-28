ba034152f0f22e8f551639f7ba6e6bf1cfc87847
Alejandro Pech <jesuspech510@gmail.com>
2022-10-28 00:14:14 -0500

    se agregaron las pruebas

---
+++ CrudEmpleados.Test/BasePruebas.cs
@@ -0,0 +1,22 @@
+﻿using CrudEmpleados.Infraestructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudEmpleados.Test
+{
+    public class BasePruebas
+    {
+        protected AppDbContext ConstruirContext(string nombreDB)
+        {
+            var opciones = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+            var dbContext = new AppDbContext(opciones);
+
+            return dbContext;
+        }
+    }
+}
